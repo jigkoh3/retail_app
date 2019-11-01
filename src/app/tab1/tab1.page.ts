d3bbbf6c007e7da42ce006909af958c5dc9d3fa1
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2019-11-01 13:11:24 +0700

    delete example file

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HomeService } from '../services/home.service';
 
 @Component({
   selector: 'app-tab1',
@@ -8,22 +7,6 @@ import { HomeService } from '../services/home.service';
 })
 export class Tab1Page {
 
-  campaigns: any;
-  shortcuts: any;
-  slideOpts = {
-    initialSlide: 1,
-    speed: 400,
-    autoplay: true
-  };
-
-  constructor(private homeService: HomeService) { }
-
-  ionViewDidEnter() {
-    this.homeService.getHomeData().then((res : any) => {
-      this.campaigns = res.campaigns;
-      this.shortcuts = res.shortcuts;
-      
-    })
-  }
+  constructor() {}
 
 }
